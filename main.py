6e252160d6b45524b628d8d7b6834435a801ace7
Blackstar84 <carlosalmada84@gmail.com>
2023-06-06 09:39:06 -0400

    se agrega la clase de conexión bajo contexto

--- main.py
+++ main.py
@@ -14,15 +14,17 @@ if __name__ == '__main__':
     try:
         connect = pymysql.Connect(host='localhost', port=3306, user='root', passwd='root', db='pythondb')
         cursor = connect.cursor()
-        cursor.execute(DROP_TABLE_USERS)
-        cursor.execute(USERS_TABLE)
+        with connect.cursor() as cursor: 
+            
+            cursor.execute(DROP_TABLE_USERS)
+            cursor.execute(USERS_TABLE)
         
     except pymysql.err.OperationalError as err:
         print('No fue posible realizar la conexión!')   
         print(err)
     
     finally:
-        cursor.close()
+        
         connect.close()
         
         print('Conexión finalizada de forma exitosa')
