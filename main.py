fdc4dd213b7dce6d774439d3386f14ca869d53df
Blackstar84 <carlosalmada84@gmail.com>
2023-06-06 09:04:50 -0400

    se agrega texto

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pymysql
 
 if __name__ == '__main__':
     try:
-        connect = pymysql.Connect(host='localhost', port=3306, user='root', passwd='root', db='pythondb')
+        connect = pymysql.Connect(host='localhost', port=3306, user='roota', passwd='root', db='pythondb')
         print('Conexión realizada de forma exitosa!')
     except pymysql.err.OperationalError as err:
         print('No fue posible realizar la conexión!')   
