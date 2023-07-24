b7bc04c8da99b0c2f46b4029245d394542c53275
Blackstar84 <carlosalmada84@gmail.com>
2023-07-24 11:10:00 -0400

    se agrega hasta la clase de borrar usuario

--- main.py
+++ main.py
@@ -28,14 +28,51 @@ def list_users(connect, cursor):
 
 def update_user(connect, cursor):
     """C) Actualizar usuario"""
-    pass
+    id = input("Ingresa el id del usuario a actualizar: ")
+    
+    query = "SELECT id FROM users WHERE id= %s"
+    cursor.execute(query, (id,))
+    
+    user = cursor.fetchone() # None
+    
+    if user:
+        username = input("Ingresa un nuevo username: ")
+        email = input("Ingresa un nuevo email: ")
+        
+        query = "UPDATE users SET username = %s, email = %s WHERE id = %s"
+        values = (username, email, id)
+        
+        cursor.execute(query, values)
+        connect.commit()
+        
+        print(">>> Usuario actualizado exitosamente.")
+    else:
+        print(">>> No existe un usuario con ese id, intenta de nuevo")    
+        
 
 def delete_user(connect, cursor):
     """D) Eliminar usuario"""
-    pass 
+    
+    id = input("Ingresa el id del usuario a eliminar: ")
+    
+    query = "SELECT id FROM users WHERE id= %s"
+    cursor.execute(query, (id,))
+    
+    user = cursor.fetchone() # None
+    
+    if user:
+        query = "DELETE FROM users WHERE id = %s"
+        
+        cursor.execute(query, (id,))
+        connect.commit()
+        
+        print(">>> Usuario borrado exitosamente!")
+    else:
+        print(">>> No existe un usuario con ese id, intenta de nuevo")      
+     
 
 def default(*args):
-    print("Opción no valida")
+    print(">>> Opción no valida")
 
 if __name__ == '__main__':
     
